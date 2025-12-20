f7a86576d0ff8bb226f7cfb67d9dce49de35947e
Edgar Mendes <mendesbr92@gmail.com>
2025-12-20 17:20:50 -0300

    feat: criado a classe de constantes com o contrutor private

--- app/src/main/java/br/com/zpx/App.java
+++ app/src/main/java/br/com/zpx/App.java
@@ -1,6 +1,8 @@
 package br.com.zpx;
 
 import br.com.zpx.petshop.domain.PetMachine;
+import br.com.zpx.petshop.service.PetMachineService;
+import br.com.zpx.petshop.service.PetMachineServiceImp;
 
 /**
  * Hello world!
@@ -10,10 +12,13 @@ public class App {
     public static void main(String[] args) {
 
         PetMachine petMachine = new PetMachine();
+        PetMachineService service = new PetMachineServiceImp(petMachine);
 
-        petMachine.setNivelAtualAgua(30);
-        petMachine.setNivelAtualShampoo(10);
-        petMachine.setQuantAtualPet(0);
-        petMachine.setPetEstaLimpo(false);
+        petMachine.setNivelAtualAgua(28);
+        // petMachine.setNivelAtualShampoo(10);
+        // petMachine.setQuantAtualPet(0);
+        // petMachine.setPetEstaLimpo(false);
+
+        service.abastecerAgua();
     }
 }
